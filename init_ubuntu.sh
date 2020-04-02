d7373ea574e978c15759ff39c71b9bb17c91a728
Jonathan Earl <earl.jonathan@gmail.com>
2020-04-02 10:54:29 -0700

    add unbuntu restore script

---
+++ init_ubuntu.sh
@@ -0,0 +1,22 @@
+cd ~/Downloads
+
+sudo apt update
+sudo apt upgrade
+
+# git
+apt-get install git
+git --version
+
+# vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install code # or code-insiders
+code --version
+
+# node
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
+node --version

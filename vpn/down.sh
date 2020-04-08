558605246062b4ef9fbd6b79bc5e36055f78205b
whattheearl <earl.jonathan@gmail.com>
2020-04-08 14:19:08 -0700

    add scripts for start and stopping vpn

---
+++ vpn/down.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# find network name using "nmcli con"
+# {network manager cli} {connection} {down} id {networkname}
+nmcli con down id TylerTech
---
+++ vpn/up.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# find network name using "nmcli con"
+
+# {network manager cli} {connection} up id {networkname}
+nmcli con up id TylerTech
